b47ddd3621f3fc07db089fab2345d9b375f8a42b
Alesouza7 <alexcostasouza86@yahoo.com>
2022-07-19 19:37:20 -0300

    B-Alex-Souza

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+const resp1 = prompt("1)Você ja atua na area de tecnologia, responda Sim ou Não?")
+const resp2 = prompt("2)Está tendo dificuldades nas aulas da Labenu, responda Sim ou Não?")
+const resp3 = prompt("3)Você cursa alguma faculdade, responda Sim ou Não?")
+
+console.log("Resposta 1=",resp1)
+console.log("Resposta 2=",resp2)
+console.log("Resposta 3=",resp3)
+
+const perg1 = "1)Você ja atua na area de tecnologia, responda Sim ou Não?"
+const perg2 = "2)Está tendo dificuldades nas aulas da Labenu, responda Sim ou Não?"
+const perg3 = "3)Você cursa alguma faculdade, responda Sim ou Não?"
+
+console.log(perg1,"-",resp1)
+console.log(perg2,"-",resp2)
+console.log(perg3,"-",resp3)
---
+++ exercicio3/script.js
@@ -0,0 +1,12 @@
+let a = 10
+let b = 25
+
+console.log("A-",a)
+console.log("B-",b)
+
+c = a
+a = b
+b = c
+
+console.log("A-",a)
+console.log("B-",b)
